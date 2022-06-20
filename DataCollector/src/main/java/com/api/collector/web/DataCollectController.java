5f42c73e0651f2b302d1832f4130a78316eaba12
Changmini <cml039465@gmail.com>
2022-06-20 22:08:11 +0900

    again project

--- DataCollector/src/main/java/com/api/collector/web/DataCollectController.java
+++ DataCollector/src/main/java/com/api/collector/web/DataCollectController.java
@@ -19,25 +19,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import egovframework.rte.fdl.property.EgovPropertyService;
-import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
-import javax.annotation.Resource;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springmodules.validation.commons.DefaultBeanValidator;
 
 import com.api.collector.service.DCollector;
 
@@ -52,7 +38,7 @@ public class DataCollectController {
 	@RequestMapping(value = "/test.do")
 	public String callCollectors() {
 		String URL = "https://www.airbnb.co.kr/s/homes?refinement_paths%5B%5D=%2Fhomes&date_picker_type=flexible_dates&search_mode=flex_destinations_search&search_type=AUTOSUGGEST";
-		List<WebElement> contents = dc.collect(URL, "div.c12h3gv8.dir.dir-ltr");
+		List<WebElement> contents = dc.collect(URL, "div.cy5jw6o.dir.dir-ltr"); 
 		
 		int cSize = contents.size();
 		String[] hrefArr = new String[cSize];
@@ -62,17 +48,18 @@ public class DataCollectController {
 		    // 에어비앤비 숙박 리스트 가져오기
 		    for(WebElement content : contents ) {
 		        try {
-		        	// 숙박 URL 정보
+		        	// 숙박 URL 정보 
+		        	// content의 HTML을 확인할 때, content.getAttribute("innerHTML"));
 		            hrefArr[idx++] = content.findElement(By.cssSelector("a")).getAttribute("href");
-		            
 		        } catch ( NoSuchElementException e ) {
-		            // pass
+		        	System.out.println("Search Main Page");
+		        	e.printStackTrace();
 		        }
 		    }
 		}
 		
 		System.out.println(Arrays.toString(hrefArr));
-		
+		/*
 		for (int i = 0; i < hrefArr.length; i++) {
 			List<WebElement> conts = dc.collect(hrefArr[i], "data url");
 			
@@ -87,8 +74,11 @@ public class DataCollectController {
 				}
 			}
 		}
+		*/
 		
-		
+		// method에 집어넣자
+		// driver.close(); close를 통해 모든 탭이 종료되었을 때, WebDriver도 닫히게 되는데 표면적으로는 종료된 것처럼 보이지만 Process가 살아있어서 자원의 낭비를 유발함.
+		// driver.quit(); close로 모든 탭을 종료시켰을 때와는 다르게 Process 자체를 종료함.
 		System.out.println("success");
 		return "test";
 	}
